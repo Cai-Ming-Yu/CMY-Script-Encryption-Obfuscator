b5178e41a1dd731ef6f990f40a262a3f10617aa3
彩铭羽 <148033104+Cai-Ming-Yu@users.noreply.github.com>
2023-10-17 12:25:53 +0800

    ねね
    ねね

---
+++ Base.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+: このスクリプトは彩铭羽が独自に作成したものです。このプロジェクトをサポートしていただける方は、私のGitHubリポジトリ https://github.com/Cai-Ming-Yu/CMY-Script-Obfuscator にアクセスして、私に星をつけてください！
+
+clear;
+
+function getInputFile () {
+    printf "ファイルパスを入力(Input file dir)：";
+    read input;
+    [[ ! -f "${input}" ]] && {
+        echo "ファイルが存在しない(Not a real file)";
+        exit 1;
+    }
+    return 0;
+}
+
+function getOutputFile () {
+    printf "出力パスを入力(output file dir)：";
+    read output;
+    [[ -e "${output}" ]] && {
+        [[ -f "${output}" ]] && {
+            echo "警告、ファイルはすでに存在します(Warning, file already exist)";
+        } || {
+            echo "パスが利用できません(Not available)";
+            exit 1;
+        }
+    }
+    return 0;
+}
+
+function makeNewFile () {
+    local base="{ \$(printf \"$(printf "eval"|base64 -w 0)\"|base64 -d) \"\$(echo -en \"$(cat "${input}"|gzip -c9|base64 -w 0)\"|base64 -d|zcat)\";};";
+    local shell="{ \$(printf \"$(printf "eval"|base64 -w 0)\"|base64 -d) \"\$(echo -en \"$(echo -en "${base}"|base64 -w 0)\"|base64 -d)\";};";
+    [[ -f "${output}" ]] && {
+        chattr -i "${output}" >/dev/null 2>&1;
+        rm -rf "${output}" >/dev/null 2>&1;
+        [[ -f "${output}" ]] && {
+            echo "ファイルを削除できない(Cannot delete file)";
+            exit 1;
+        }
+    }
+    echo -en "#!/bin/sh
+: << 彩铭羽
+
+彩铭羽 Visual Obfuscator
+
+バージョン番号: Version 1.0.0 (2023.10.16)
+
+著者: 彩铭羽 (GitHubホーム: https://github.com/Cai-Ming-Yu)
+
+GitHub Repo: https://github.com/Cai-Ming-Yu/CMY-Visual-Obfuscator
+
+使用日: $(date '+%x %A %r')
+
+This file uses Visual Obfuscator for encryption obfuscates the file, please DO NOT modify the file!
+
+彩铭羽
+
+" > "${output}";
+    echo -en "${shell}" | while read -n 1 code;do [[ "${code}" == '' || "${code}" == ' ' ]] && echo ' \' >> "${output}" || echo "${code}\\" >> "${output}";done;
+    echo -en ": '著者: 彩铭羽 (GitHubホーム: https://github.com/Cai-Ming-Yu)';" >> "${output}";
+    echo "OK!";
+    return 0;
+}
+
+eval getInputFile;
+eval getOutputFile;
+eval makeNewFile;
+exit 0;
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# CMY-Script-Obfuscator
+# 彩铭羽 Visual Obfuscator
 あなたのShellコードを視覚的に難読化する。
+
+This tool can help you obfuscate and encrypt shell code scripts to help improve security.
+However, it should still be noted that this project is just a simple example, and it does not bring good protection.
+
+## How to use
+First, download the "Base.sh" file from this project.
+Then start the script by means such as ```sh /path/to/base.sh```.
+Then you need to type the path of the script that needs to be encrypted, then the path to the output file.
+And finally, wait for the output "OK!" Can.
+
+
+## Warning
+- Do not use on all kinds of harmful files, 彩铭羽 will not be responsible for any harmful time caused by the use of this tool.
+- Do not use them on binary files unless you convert them to strings or place them at the end of the encrypted file.
+- Do not use in a production environment.
+- If it works well, order a star for the project.
